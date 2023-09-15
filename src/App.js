4e53296905989d0912636ac0c3898099e0268b06
unknown <menyhart.b.norbert@gmail.com>
2023-09-15 18:38:13 +0200

    finsihed project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [ripples, setRipple] = useState([]);
+
+  const handleRippleEffect = (e) => {
+    // window.alert("ripple");
+    const rippleContainer = e.currentTarget.getBoundingClientRect();
+
+    const size = Math.max(rippleContainer.width, rippleContainer.height);
+
+    const x = e.clientX - rippleContainer.left - size / 2;
+    const y = e.clientY - rippleContainer.top - size / 2;
+
+    const newRipple = {
+      x,
+      y,
+      size,
+      id: Date.now(),
+    };
+
+    setRipple([...ripples, newRipple]);
+  };
   return (
     <>
-      <h1>Project</h1>
+      <div className="ripple-container" onClick={handleRippleEffect}>
+        <div className="ripple">
+          {ripples.map((ripple) => (
+            <span
+              key={ripple.id}
+              style={{
+                top: ripple.y,
+                left: ripple.x,
+                width: ripple.size,
+                height: ripple.size,
+              }}
+            ></span>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
